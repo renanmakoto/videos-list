1fbb91d969c632eee68a092895388a06bcb69599
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-01-15 07:00:29 -0300

    Add files via upload

---
+++ front/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetch('/api/videos').then(res => res.json()).then(data => {
+      setVideos(data)
+    })
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {videos.map((id, index) => {
+          return(
+            <a className="videos-bloc" key={index} href={`https://youtube.com/watch?v=${id}`}>
+              <img alt="justanimage" src={`https://img.youtube.com/vi/${id}/0.jpg`}></img>
+            </a>
+          )
+        })}
+      </header>
+    </div>
+  );
+}
+
+export default App;
